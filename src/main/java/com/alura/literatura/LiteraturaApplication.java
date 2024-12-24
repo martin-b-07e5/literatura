76dd1a9bf9330bb2cd65aadbfea5be4b2fd391ee
martin-b-07e5 <martin.bergagno@gmail.com>
2024-12-23 23:54:12 -0300

    second commit

--- src/main/java/com/alura/literatura/LiteraturaApplication.java
+++ src/main/java/com/alura/literatura/LiteraturaApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraturaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraturaApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LiteraturaApplication.class, args);
+
+    // implementación del código para buscar libros por título
+
+  }
 
 }
